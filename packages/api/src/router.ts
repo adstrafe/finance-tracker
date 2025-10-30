ad7b5825f217301e3b90b2555ef2fec9136d71d9
ADstrafe <vojtech.lahuta@gmail.com>
2025-10-30 23:54:19 +0100

    chore: add forgotten transactionRouter

--- packages/api/src/router.ts
+++ packages/api/src/router.ts
@@ -1,8 +1,10 @@
 import { createTRPCRouter } from './trpc/trpc';
 import { authRouter } from './routers/auth/auth';
+import { transactionRouter } from './routers/transaction/transaction';
 
 export const appRouter = createTRPCRouter({
-	auth: authRouter
+	auth: authRouter,
+	transaction: transactionRouter
 });
 
 export type AppRouter = typeof appRouter;
